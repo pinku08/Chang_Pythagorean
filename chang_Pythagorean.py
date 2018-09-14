338b852c16eb8c2993965d90cee840d9c9de5940
Shrey Chang <shreycha.61406@redlandsschools.net>
2018-09-14 15:26:02 +0000

    add string

---
+++ chang_Pythagorean.py
@@ -0,0 +1,14 @@
+name=input('What is you name?:')
+
+print ("hello" + name)
+
+
+print("Input lengths of shorter triangle sides:")
+#Input length for a and b to find what c is.
+
+a = float(input("a: "))
+b = float(input("b: "))
+
+c = (a**2 + b**2)**(1/2)
+
+print('The length of' str(c) 'is' )
